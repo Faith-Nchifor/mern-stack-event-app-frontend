f02dfbd5a8f30f1e7609eebb269b8b8d9914a3fa
Faith-Nchifor <52848633+Faith-Nchifor@users.noreply.github.com>
2020-07-27 22:56:19 +0100

    Add files via upload

---
+++ build/precache-manifest.254c49122a1b1c7e27b728ade5da2796.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e253b2376086b65953d906b2c70cd90d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7489231ca49d3e8edf7e",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "22d904f5e066e70179d0",
+    "url": "/static/css/main.647c24ed.chunk.css"
+  },
+  {
+    "revision": "7489231ca49d3e8edf7e",
+    "url": "/static/js/2.8a35da32.chunk.js"
+  },
+  {
+    "revision": "edbc79938c564134066dc08722bf82e3",
+    "url": "/static/js/2.8a35da32.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "22d904f5e066e70179d0",
+    "url": "/static/js/main.941e0b8a.chunk.js"
+  },
+  {
+    "revision": "86c3b8054dcc8233e940",
+    "url": "/static/js/runtime-main.fc4f9221.js"
+  },
+  {
+    "revision": "63452e77aa003178e6a5c63d50fb260e",
+    "url": "/static/media/airp.63452e77.jpg"
+  }
+]);
